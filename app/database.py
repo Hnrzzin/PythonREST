3c436ae39299001bd4d6d8765539ec2470595e50
Hrnzzin <henriqueteixeira213@gmail.com>
2025-07-18 17:00:24 -0300

    Inicio do projeto REST

---
+++ app/database.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+def entrarBanco():
+   
+    try:
+        global conexao
+        conexao = mysql.connector.connect(
+            host='localhost',   
+            user='root',
+            password='Henry45*1',
+            database='univap',
+            auth_plugin ='mysql_native_password'
+            )
+
+        if conexao.is_connected():
+            global tbltemp
+            infbanco = conexao.get_server_info()
+            print(f'Conectado ao MySQL Server versão {infbanco}')
+            print('Conexão bem-sucedida!')
+            tbltemp = conexao.cursor()
+            tbltemp.execute('SELECT DATABASE()')
+            nameBanco = tbltemp.fetchone()
+            for nome in nameBanco:
+                print(f'Você está conectado ao banco de dados: {nome}')
+            print ('--'*60)
+            global tabletupla
+        else:
+            print('Erro ao conectar ao banco de dados.')
+
+    except Exception as error:
+        print(f'Erro ao conectar ao banco de dados: {error}')
---
+++ otherinf/db.sql
@@ -0,0 +1,2 @@
+insert into info (nome,telefone,email) values
+("José", "12981028919","josea@gmail.com")
